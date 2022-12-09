a1868bbd4571f7ddc75cb23f51a47bbe36c5d1ac
Peter Ludemann <peter.ludemann@gmail.com>
2022-12-09 15:39:08 +0100

    Add syntax_error call + small cleanup

--- 7.pl
+++ 7.pl
@@ -11,6 +11,7 @@
 
 solve(Path, Filesys) :-
     phrase_from_file(lines([], [], Files), Path),
+    !,
     maplist(writeln, Files),
     foldl(filesys, Files, [dir('/',[])], Filesys0),
     normalize_tree(Filesys0, Filesys),
@@ -19,29 +20,42 @@ solve(Path, Filesys) :-
     print_term(Filesys, [indent_width(2), tab_width(0), right_margin(40)]).
 
 filesys(file(Dir,FileName,Size), DirFiles0, DirFiles) :-
-    tree_to_dir(Dir, file(FileName,Size), DirFiles0, DirFiles).
+    filesys_(Dir, file(FileName,Size), DirFiles0, DirFiles).
 
-tree_to_dir([], file(FileName,Size), DirFiles, [file(FileName,Size)|DirFiles]) :-
+filesys_([], file(FileName,Size), DirFiles, [file(FileName,Size)|DirFiles]) :-
     (   member(file(FileName,Size2), DirFiles)
     ->  assert(Size == Size2)
     ;   true
     ).
-tree_to_dir([Dir|Dirs], FileAndSize, DirFiles0, [dir(Dir,SubDirFiles2)|DirFiles1]) :-
+filesys_([Dir|Dirs], FileAndSize, DirFiles0, [dir(Dir,SubDirFiles2)|DirFiles1]) :-
     (   select(dir(Dir,SubDirFiles), DirFiles0, DirFiles1)
     ->  true
     ;   SubDirFiles = [],
         DirFiles1 = DirFiles0
     ),
-    tree_to_dir(Dirs, FileAndSize, SubDirFiles, SubDirFiles2).
+    filesys_(Dirs, FileAndSize, SubDirFiles, SubDirFiles2).
 
+% Parsing the file of commands and outputs
+
+%! lines(+CurrDir:list(atom), +Files0:list, Files:list)// is det.
+
+% Files0, Files are an input-output pair, each being a list of the
+% form file(Dir,Path,Size), where Dir is a list of atoms that gets to
+% the directory (e.g., "/foo/bar/zot.txt") would produce
+% file([/,foo,bar],'zot.txt',1234).
+% CurrDir is of the same form as the Dir in a file.
+% The DCG takes a list of codes that contain the commands ("cd", "ls")
+% and outputs (with "\n" (code 10). It is designed to be used with
+% phrase_from_file/3, so that syntax_error//1 can be used.
 lines(CurrDir, Files0, Files) -->
-    "$", white, whites, "cd", white, whites, nonblanks(Dir0), blanks_to_nl,
+    "$", white, whites, "cd",
+    white, whites, nonblanks(Dir0), blanks_to_nl,
     { atom_codes(Dir, Dir0) },
-    !,
     { (   Dir = '..'
       ->  append(CurrDir2, [_Last], CurrDir)
       ;   append(CurrDir, [Dir], CurrDir2)
-    ) },
+      )
+    },
     !,
     lines(CurrDir2, Files0, Files).
 lines(CurrDir, Files0, Files) -->
@@ -50,6 +64,8 @@ lines(CurrDir, Files0, Files) -->
     !,
     lines(CurrDir, Files1, Files).
 lines(_CurrDir, Files, Files) --> [].
+lines(_CurrDir, Files, Files) -->
+    syntax_error("Invalid command").
 
 ls(CurrDir, Files0, Files) -->
     \+ "$",
@@ -68,7 +84,7 @@ ls(CurrDir, Files0, Files) -->
 ls(_CurrDir, Files, Files) --> [].
 
 normalize_tree(file(File,Size), file(File,Size)).
-normalize_tree(dir(Dir, DirFiles), dir(Dir, SortedNormalizedDirFiles)) :-
+normalize_tree(dir(Dir,DirFiles), dir(Dir,SortedNormalizedDirFiles)) :-
     maplist(normalize_tree, DirFiles, NormalizedDirFiles),
     msort(NormalizedDirFiles, SortedNormalizedDirFiles).
 normalize_tree([F|Fs], Sorted) :-
--- 7.test.pl
+++ 7.test.pl
@@ -13,6 +13,26 @@ test_7 :-
 
 :- begin_tests('7').
 
+expected_tree_new(Tree) :-
+    normalize_tree([dir('/',
+                        [ dir('a',
+                              [ dir(['/','a'], 'e',
+                                    [ file(['/','a','e'], 'i', 584)
+                                    ]),
+                                file(['/','a'], 'f', 29116),
+                                file(['/','a'], 'g', 2557),
+                                file(['/','a'], 'h.lst', 62596)
+                              ]),
+                          file(['/'], 'b.txt', 14848514),
+                          file(['/'], 'c.dat', 8504156),
+                          dir(['/'], 'd',
+                              [ file(['/', 'd'], 'j', 4060174),
+                                file(['/', 'd'], 'd.log', 8033020),
+                                file(['/', 'd'], 'd.ext', 5626152),
+                                file(['/', 'd'], 'k', 7214296)
+                              ])
+                        ])],
+                   Tree).
 expected_tree(Tree) :-
     normalize_tree([dir('/',
                         [ dir('a',
@@ -24,13 +44,13 @@ expected_tree(Tree) :-
                                 file('h.lst', 62596)
                               ]),
                           file('b.txt', 14848514),
-                          file('c.dat', 8504156),
+                         file('c.dat', 8504156),
                           dir('d',
                               [ file('j', 4060174),
                                 file('d.log', 8033020),
                                 file('d.ext', 5626152),
                                 file('k', 7214296)
-                              ])
+                             ])
                         ])],
                    Tree).
 
